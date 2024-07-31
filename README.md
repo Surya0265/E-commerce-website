1350076042fa98c9b8fb5a697fa300a8a59d4465
Surya0265 <suryaprakashb265@gmail.com>
2024-08-01 01:07:00 +0530

    ok

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # E-commerce-website
+ to 1.run cd "backend"
+    node app.js
+    new terminal:
+    2.cd "frontend"
+    npm start
---
+++ backend/app.js
@@ -0,0 +1,29 @@
+const express=require('express')
+const app=express()
+const dotenv=require('dotenv')
+const path=require('path')
+dotenv.config({path:path.join(__dirname,'config','config.env')})
+const products=require('./routes/product')
+const order=require('./routes/order')
+const connectDatabase=require('./config/connectDatabase')
+const cors=require('cors')
+
+
+connectDatabase()
+app.use(express.json())
+app.use(cors())
+app.use('/api/v1',products)
+app.use('/api/v1',order)
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server  listening to ${process.env.PORT}  in ${process.env.NODE_ENV} `)
+})
